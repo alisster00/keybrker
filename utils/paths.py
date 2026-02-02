3f84e2603bdded406a3c2affa5b0ceeec8244124
alister00 <akaristr@protonmail.com>
2026-02-02 11:14:56 -0500

    mejora: los diccionarios se generan en el directorio 'data'

--- utils/paths.py
+++ utils/paths.py
@@ -1,3 +1,10 @@
 from pathlib import Path
 
 project_root = Path(__file__).resolve().parent.parent
+
+data_dir = project_root / "data"
+
+dictionaries_dir = data_dir / "dictionaries"
+
+default_dict_path = dictionaries_dir / "dictionary.txt"
+target_pass_path = data_dir / "output_password.txt"
